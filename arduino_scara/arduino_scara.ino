867885bc773524da1a0c81291e9af0cecd47a620
DDanila1 <152435358+DDanila1@users.noreply.github.com>
2024-11-10 17:08:05 +0300

    Update №2 arduino code

--- arduino_scara/arduino_scara.ino
+++ arduino_scara/arduino_scara.ino
@@ -10,8 +10,8 @@
 AccelStepper stepper1(AccelStepper::DRIVER, STEP_PIN_1, DIR_PIN_1);
 AccelStepper stepper2(AccelStepper::DRIVER, STEP_PIN_2, DIR_PIN_2);
 
-const double L1 = 245;  //-----------------передаю константные значения длин звеньев
-const double L2 = 234526;
+const double L1 = 15;  //-----------------передаю константные значения длин звеньев
+const double L2 = 15;
 
 //-----------------------------------------ФУНКЦИЯ ОПРЕДЕЛЕНИЯ УГЛОВ
 void MoveFromStartToBoltFeeder(double x, double y, double& theta1, double& theta2)  //------передача в функцию аргументов
@@ -33,7 +33,7 @@ void MoveFromStartToBoltFeeder(double x, double y, double& theta1, double& theta
   }
 
   else {
-    double cosTheta2 = cos((L1 * L1 + L2 * L2 - r * r) / (2 * L1 * L2));  //--------расчет косинуса угла№ 2
+    double cosTheta2 = cos(L1 * L1 + L2 * L2 - r * r) / (2 * L1 * L2);  //--------расчет косинуса угла№ 2
 
     if (cosTheta2 < -1 || cosTheta2 > 1)  //-------проверка на значение косинуса. В случае другого значения косинуса, не входящего в диапозон -1 до 1, значения улов станут 0
     {
@@ -64,7 +64,7 @@ void MoveFromBoltFeederToObject(double obj_x, double obj_y, double& new_theta1, 
   }
 
   else {
-    double new_cosTheta2 = cos((L1 * L1 + L2 * L2 - r * r) / (2 * L1 * L2));
+    double new_cosTheta2 = cos(L1 * L1 + L2 * L2 - r * r) / (2 * L1 * L2);
 
     if (new_cosTheta2 < -1 || new_cosTheta2 > 1) {
       new_theta1 = new_theta2 = 0;
